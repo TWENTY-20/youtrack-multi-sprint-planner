1638512ba80f3f4e77cb503ef0cf8d4a5a6e2e3c
Silas Porth <silas.porth@twenty20.de>
2024-07-30 15:20:07 +0200

    New build pipeline for backend.js

--- vite.config.ts
+++ vite.config.ts
@@ -3,33 +3,53 @@ import react from "@vitejs/plugin-react";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    root: "src/widgets",
-    plugins: [react(), viteStaticCopy({
-        targets: [
-            { src: "src/backend.js", dest: ".." },
-            { src: "src/logo.svg", dest: ".." },
-            { src: "src/manifest.json", dest: ".." },
-            { src: "src/settings.json", dest: ".." },
-        ]
-    })],
-    build: {
-        outDir: "../../build/widgets",
-        assetsDir: "",
-        assetsInlineLimit: 0,
-        emptyOutDir: true,
-        sourcemap: false, // enable for debugging purposes
-        rollupOptions: {
-            output: {
-                entryFileNames: "[name].js",
-                chunkFileNames: "[name].js",
-                assetFileNames: "[name].[ext]"
+export default defineConfig(({ mode }) => {
+    if (mode === "backend") {
+        return {
+            build: {
+                lib: {
+                    entry: "./src/backend.js",
+                    formats: ["cjs"],
+                    name: "backend",
+                    fileName: "backend.js"
+                },
+                rollupOptions: {
+                    output: {
+                        entryFileNames: "backend.js",
+                        dir: "build"
+                    }
+                }
             }
-        }
-    },
-    esbuild: {
-        supported: {
-            "top-level-await": true
-        }
-    },
+        };
+    }
+
+    return {
+        root: "src/widgets",
+        plugins: [react(), viteStaticCopy({
+            targets: [
+                { src: "../../src/logo.svg", dest: ".." },
+                { src: "../../src/manifest.json", dest: ".." },
+                { src: "../../src/settings.json", dest: ".." },
+            ]
+        })],
+        build: {
+            outDir: "../../build/widgets",
+            assetsDir: "",
+            assetsInlineLimit: 0,
+            emptyOutDir: true,
+            sourcemap: false, // enable for debugging purposes
+            rollupOptions: {
+                output: {
+                    entryFileNames: "[name].js",
+                    chunkFileNames: "[name].js",
+                    assetFileNames: "[name].[ext]"
+                }
+            }
+        },
+        esbuild: {
+            supported: {
+                "top-level-await": true
+            }
+        },
+    };
 });
